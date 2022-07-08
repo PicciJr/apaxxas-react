d8159b60b98be6b279bfa4876b68bf4a10ed204a
PicciJr <piccijunior2319@gmail.com>
2022-07-08 08:57:41 +0200

    print deposit details with subjects attached

--- src/ui/screens/DepositDetail.tsx
+++ src/ui/screens/DepositDetail.tsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Deposit } from '../../domain/deposit';
 import { useGetDeposit } from '../../application/getDeposit';
 import { useParams } from 'react-router-dom';
+import { useGlobalContext } from '../../services/globalContext';
 
 function DepositDetail() {
+  const { user: loggedInUser } = useGlobalContext();
   const [deposit, setDeposit] = useState<Deposit | null>(null);
   const params = useParams();
   useEffect(() => {
@@ -16,7 +18,34 @@ function DepositDetail() {
   }, []);
 
   return (
-    <div className="h-screen px-4 py-2 overflow-scroll">DepositDetail</div>
+    <div className="h-screen px-4 py-2 overflow-scroll">
+      <div className="w-full text-white rounded-md bg-apxpurple-100">
+        {deposit && (
+          <ul className="px-2 py-4">
+            <>
+              {deposit.expenses.map((expense) => {
+                return expense.debtors.map((debtor) => {
+                  return (
+                    debtor.id !== loggedInUser.id && (
+                      <li>
+                        <span className="font-bold">{debtor.alias}</span> me
+                        debe {expense.total}$
+                        <ul className="px-8 list-disc">
+                          <li>
+                            <span className="italic">{expense.subject}</span>:{' '}
+                            {expense.total}$
+                          </li>
+                        </ul>
+                      </li>
+                    )
+                  );
+                });
+              })}
+            </>
+          </ul>
+        )}
+      </div>
+    </div>
   );
 }
 
