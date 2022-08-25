fc80a189dd666a8c91b8ba961534d8aa9dbe7e1c
PicciJr <piccijunior2319@gmail.com>
2022-08-25 08:15:02 +0200

    avoid home screen if user logged in

--- src/ui/screens/Home.tsx
+++ src/ui/screens/Home.tsx
@@ -6,16 +6,17 @@ import { useAuthenticate } from '../../application/authenticate';
 import { NotFoundError } from '../../domain/Errors/NotFoundError';
 import { useGlobalContext } from '../../services/globalContext';
 import { useNavigate } from 'react-router-dom';
+import { Spinner } from '@chakra-ui/react';
 
 export default function HomeScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { user, updateUser } = useGlobalContext();
+  const { user, updateUser, isPending } = useGlobalContext();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (user) navigate('/resumen');
-  }, []);
+  }, [user]);
 
   const handleUserLogin = async () => {
     try {
@@ -38,29 +39,42 @@ export default function HomeScreen() {
 
   return (
     <div className="absolute flex flex-col justify-center px-8 pb-24 overflow-scroll transform -translate-x-1/2 translate-y-1/2 left-1/2">
-      <Box mb={1}>
-        <ATextInput placeholder="Email" onInputHandler={setEmail} />
-      </Box>
-      <Box mb={4}>
-        <ATextInput
-          placeholder="Password"
-          onInputHandler={setPassword}
-          type="password"
+      {isPending && (
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
         />
-      </Box>
-      <AButton
-        text="Iniciar sesión"
-        color="purple"
-        clickHandler={handleUserLogin}
-      />
-      <Box mb={4}></Box>
-      <hr className="mb-8" />
-      <AButton
-        text="Iniciar sesión con Google"
-        color="purple"
-        clickHandler={handleGoogleLogin}
-        variant="outline"
-      />
+      )}
+      {!isPending && (
+        <>
+          <Box mb={1}>
+            <ATextInput placeholder="Email" onInputHandler={setEmail} />
+          </Box>
+          <Box mb={4}>
+            <ATextInput
+              placeholder="Password"
+              onInputHandler={setPassword}
+              type="password"
+            />
+          </Box>
+          <AButton
+            text="Iniciar sesión"
+            color="purple"
+            clickHandler={handleUserLogin}
+          />
+          <Box mb={4}></Box>
+          <hr className="mb-8" />
+          <AButton
+            text="Iniciar sesión con Google"
+            color="purple"
+            clickHandler={handleGoogleLogin}
+            variant="outline"
+          />
+        </>
+      )}
     </div>
   );
 }
